35396dcf436fe9f671e9317f5ab96d03f9be2c21
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2026-03-14 22:16:32 +0530

    Update machines.js

--- frontend/src/api/machines.js
+++ frontend/src/api/machines.js
@@ -1,10 +1,10 @@
-import { machineClient } from './client'
+import client from './client'
 
 export const machinesApi = {
-  getAll: (params) => machineClient.get('/api/machines', { params }),
-  getById: (id) => machineClient.get(`/api/machines/${id}`),
-  getOverdue: () => machineClient.get('/api/machines/overdue'),
-  create: (data) => machineClient.post('/api/machines', data),
-  update: (id, data) => machineClient.patch(`/api/machines/${id}`, data),
-  delete: (id) => machineClient.delete(`/api/machines/${id}`),
+  getAll: (params) => client.get('/api/machines', { params }),
+  getById: (id) => client.get(`/api/machines/${id}`),
+  getOverdue: () => client.get('/api/machines/overdue'),
+  create: (data) => client.post('/api/machines', data),
+  update: (id, data) => client.patch(`/api/machines/${id}`, data),
+  delete: (id) => client.delete(`/api/machines/${id}`),
 }
